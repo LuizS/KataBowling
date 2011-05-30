04966e3f74b14cb68e703b434aef325415b48421
Luiz Scheidegger <lscscheidegger@hotmail.com>
2011-05-30 19:27:08 +0200

    19:27 autocommit after successful build

--- BowlingKata/Class1.cs
+++ BowlingKata/Class1.cs
@@ -19,6 +19,28 @@ namespace BowlingKata
 
             Assert.That(game.Score, Is.EqualTo(0));
         }
+
+        [Test]
+        public void Score_UserGetsPinsWithOnlyOnePin_ReturnsTwenty()
+        {
+            Game game = new Game();
+
+            for (int i = 1; i <= 20; i++)
+                game.Roll(1);
+
+            Assert.That(game.Score, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Score_UserGetsPinsWithNinePins_Returns180()
+        {
+            Game game = new Game();
+
+            for (int i = 1; i <= 20; i++)
+                game.Roll(9);
+
+            Assert.That(game.Score, Is.EqualTo(180));
+        }
     }
 
     public class Game
