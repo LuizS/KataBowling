9eabd58dccf6bbe7a3cb8c0f2a05419cbf8643fe
Luiz Scheidegger <lscscheidegger@hotmail.com>
2011-05-30 20:24:52 +0200

    20:24 autocommit after successful build

--- BowlingKata/Class1.cs
+++ BowlingKata/Class1.cs
@@ -57,10 +57,10 @@ namespace BowlingKata
 
         public void Roll(int i)
         {
-            if (Frames.Count() == 0 || Frames.Last().FrameIsClosed)
+            if ((Frames.Count() == 0  || Frames.Last().FrameIsClosed) && Frames.Count < 10)
             {
                 Frames.Add(new Frame(i));
-            }else if (Frames.Count() <= 10)
+            }else if(Frames.Count < 10)
             {
                 Frames.Last().AddSecondRoll(i);
             }
@@ -124,7 +124,7 @@ namespace BowlingKata
             if (BonusRolls > 0)
             {
                 this.Frame.AddBonus(i);
-                BonusRolls--;
+                BonusRolls --;
             }
             
         }
